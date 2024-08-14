d1de0926a948f3e562f7105ee681958f1a3109fa
soumya12w <soumya.sarkar@12thwonder.com>
2024-08-14 00:29:59 -0700

    blogs changed

--- src/lib/components/cards/BlogCard1.svelte
+++ src/lib/components/cards/BlogCard1.svelte
@@ -1,6 +1,9 @@
 <script lang='ts'>
     export let title: string;
     export let date: string;
+    export let duration: string;
+    export let event:string;
+    export let buttonName:string;
     export let description: string;
     export let image: string;
     export let imageAlt: string;
@@ -11,19 +14,25 @@
 
 <a href={slug}>
     <article class="flex flex-col h-full shadow-md rounded-md hover:scale-105 transition-transform hover:shadow-lg cursor-pointer group my-4">
+        <div class="w-full h-[15em] relative">
+        <span class='absolute top-2 left-2 border border-white text-white font-semibold rounded-2xl p-1'>{event}</span>    
         <img src={image} alt={imageAlt} class="w-full h-[15em] rounded-t-md bg-slate-300" />
-        <div class="card-content p-6 flex flex-col gap-2 pb-10 ">
+        <span class='absolute bottom-0 left-0 w-2/5 bg-gray-300 text-gray-700 text-center font-semibold rounded-t-lg p-2'>{duration}</span>
+        </div>
+        <div class="card-content p-4 flex flex-col gap-2 pb-10 ">
+
+            <h2 class="text-lg font-bold group-hover:text-green-500">{title}</h2>
+            <p class="text-base text-gray-700">{description}...</p>
             <div class="flex justify-between items-center" >
                 <div class="flex gap-2 items-center">
                     <div>
-                        <p class="text-md">{date}</p>
+                        <p class="text-xs text-gray-700">{date}</p>
                     </div>
                 </div>
                <!--  <i class="ri-more-2-fill"></i> -->
     
             </div>
-            <h2 class="text-xl font-bold group-hover:text-sky-500">{title}</h2>
-            <p class="text-xl">{description}...</p> 
+            <button class="bg-[#2b2b2b]  text-white text-sm hover:text-green-500 py-2 mt-4 mb-2 px-6 rounded-md md:rounded-lg shadow w-40 " >{buttonName} </button> 
         </div>
     </article>
 </a>
--- src/routes/events/+page.svelte
+++ src/routes/events/+page.svelte
@@ -3,11 +3,6 @@
     import eventcover from '$lib/assets/eventpagecover.avif';
     import BlogCard1 from '$lib/components/cards/BlogCard1.svelte';
     import { goto } from '$app/navigation'; 
-	import BlogCard from '$lib/components/cards/BlogCard.svelte';
-
-    function navigateToEvents() {
-        goto('upcoming-events'); 
-    }
 
     const blogData = [
         {
@@ -16,6 +11,9 @@
         description: 'This is a brief description of the blog content',
         image: eventimg, // Replace with actual image path
         imageAlt: 'Blog image description',
+        event:'coming soon',
+        duration:'Not Started',
+        buttonName:'VIEW DETAILS',
         slug: '/blog/sustainium-post'
         },
         {
@@ -24,6 +22,9 @@
         description: 'This is a brief description of the blog content',
         image: eventimg, // Replace with actual image path
         imageAlt: 'Blog image description',
+        event:'webinar',
+        duration:'45 min',
+        buttonName:'VIEW EVENT',
         slug: '/blog/sustainium-post'
         },
         {
@@ -32,6 +33,9 @@
         description: 'This is a brief description of the blog content',
         image: eventimg, // Replace with actual image path
         imageAlt: 'Blog image description',
+        event:'webinar',
+        duration:'45 min',
+        buttonName:'VIEW EVENT',
         slug: '/blog/sustainium-post'
         },
         {
@@ -40,6 +44,9 @@
         description: 'This is a brief description of the blog content',
         image: eventimg, // Replace with actual image path
         imageAlt: 'Blog image description',
+        event:'coming soon',
+        duration:'Not Started',
+        buttonName:'VIEW DETAILS',
         slug: '/blog/sustainium-post'
         },
         {
@@ -48,6 +55,9 @@
         description: 'This is a brief description of the blog content',
         image: eventimg, // Replace with actual image path
         imageAlt: 'Blog image description',
+        event:'webinar',
+        duration:'45 min',
+        buttonName:'VIEW EVENT',
         slug: '/blog/sustainium-post'
         },
         {
@@ -56,94 +66,143 @@
         description: 'This is a brief description of the blog content',
         image: eventimg, // Replace with actual image path
         imageAlt: 'Blog image description',
+        event:'webinar',
+        duration:'45 min',
+        buttonName:'VIEW EVENT',
         slug: '/blog/sustainium-post'
-        }
+        },
+        {
+        title: 'Sustainium Blog Title',
+        date: 'August 11, 2024',
+        description: 'This is a brief description of the blog content',
+        image: eventimg, // Replace with actual image path
+        imageAlt: 'Blog image description',
+        event:'coming soon',
+        duration:'Not Started',
+        buttonName:'VIEW DETAILS',
+        slug: '/blog/sustainium-post'
+        },
+        {
+        title: 'Sustainium Blog Title',
+        date: 'August 11, 2024',
+        description: 'This is a brief description of the blog content',
+        image: eventimg, // Replace with actual image path
+        imageAlt: 'Blog image description',
+        event:'webinar',
+        duration:'45 min',
+        buttonName:'VIEW EVENT',
+        slug: '/blog/sustainium-post'
+        },
+        {
+        title: 'Sustainium Blog Title',
+        date: 'August 11, 2024',
+        description: 'This is a brief description of the blog content',
+        image: eventimg, // Replace with actual image path
+        imageAlt: 'Blog image description',
+        event:'coming soon',
+        duration:'Not Started',
+        buttonName:'VIEW DETAILS',
+        slug: '/blog/sustainium-post'
+        },
+        {
+        title: 'Sustainium Blog Title',
+        date: 'August 11, 2024',
+        description: 'This is a brief description of the blog content',
+        image: eventimg, // Replace with actual image path
+        imageAlt: 'Blog image description',
+        event:'coming soon',
+        duration:'Not Started',
+        buttonName:'VIEW DETAILS',
+        slug: '/blog/sustainium-post'
+        },
+        {
+        title: 'Sustainium Blog Title',
+        date: 'August 11, 2024',
+        description: 'This is a brief description of the blog content',
+        image: eventimg, // Replace with actual image path
+        imageAlt: 'Blog image description',
+        event:'coming soon',
+        duration:'Not Started',
+        buttonName:'VIEW DETAILS',
+        slug: '/blog/sustainium-post'
+        },
+        {
+        title: 'Sustainium Blog Title',
+        date: 'August 11, 2024',
+        description: 'This is a brief description of the blog content',
+        image: eventimg, // Replace with actual image path
+        imageAlt: 'Blog image description',
+        event:'webinar',
+        duration:'45 min',
+        buttonName:'VIEW EVENT',
+        slug: '/blog/sustainium-post'
+        },
+        {
+        title: 'Sustainium Blog Title',
+        date: 'August 11, 2024',
+        description: 'This is a brief description of the blog content',
+        image: eventimg, // Replace with actual image path
+        imageAlt: 'Blog image description',
+        event:'webinar',
+        duration:'45 min',
+        buttonName:'VIEW EVENT',
+        slug: '/blog/sustainium-post'
+        },
+
+       
         
 ];
+    
+    let selectedTab='All'; // Default tab is "All"
+    let filteredData = blogData;
 
-let currentPage = 1;
-  const itemsPerPage = 3;
-
-  const totalPages = Math.ceil(blogData.length / itemsPerPage);
-
-  function getCurrentPageData() {
-    const start = (currentPage - 1) * itemsPerPage;
-    const end = start + itemsPerPage;
-    return blogData.slice(start, end);
-  }
-
-  function goToPage(page:any) {
-    if (page >= 1 && page <= totalPages) {
-      currentPage = page;
+    $: {
+    if (selectedTab === 'Recorded') {
+        filteredData = blogData.filter(blog => blog.event === 'webinar');
+    } else if (selectedTab === 'Upcoming') {
+        filteredData = blogData.filter(blog => blog.event !== 'webinar');
+    } else {
+        filteredData = blogData;
     }
-  }
-
-  function nextPage() {
-    if (currentPage < totalPages) {
-      currentPage += 1;
     }
-  }
 
-  function prevPage() {
-    if (currentPage > 1) {
-      currentPage -= 1;
-    }
+
+  function selectTab(tab: string) {
+    selectedTab = tab;
+    console.log(tab);
   }
 
 </script>
 
-<section class="container py-20 lg:px-40">
+<section class="container py-20 px-20">
     <div class="relative rounded-2xl overflow-hidden mb-24">
         <img src={eventcover} alt="cover" class="w-full h-[200px]  object-cover">
         <h1 class="absolute inset-0 flex items-center justify-center text-white font-bold text-3xl lg:text-5xl bg-black bg-opacity-50 p-4">
             Live Events
         </h1>
     </div> 
-
-    <h2 class="font-bold text-gray-700 text-center text-2xl lg:text-4xl  mb-16 ">Upcoming Events</h2>
-    <article class="mt-6 mb-6 sm:flex lg:flex-row sm:flex-col justify-between items-start w-full rounded-2xl border border-gray-300 shadow-lg shadow-gray-300 overflow-hidden">
-        <div class='sm:w-full md:w-1/3'>
-            <img class='h-[300px] w-full' src={eventimg} alt='event'>
-        </div>
-        <div class='sm:w-full md:w-2/3 flex flex-col justify-center text-start py-14'>
-            <h3 class='font-semibold text-gray-500 md:text-lg text-xs px-6 my-2 '>August 13, 2024 - 9:30am to 5:30pm Central Daylight Time</h3>
-            <h3 class='font-bold text-[rgb(83,212,40)] md:text-3xl text-xl px-6 mt-2 mb-2  '>Software Developer Success</h3>
-            <p class='md:text-md text-sm px-6 mb-2'>Learn the crucial technical and interpersonal skills needed to thrive as a Junior Engineer.</p>
-            <a href='xyz'>
-                <button class="bg-[#2b2b2b]  text-white hover:text-green-500 py-2 mx-6 mt-4 mb-2 px-6 rounded-full md:rounded-3xl shadow w-40 self-center">
-                View Event
-            </button>
-            </a>
-        </div>
-    </article>
-
-
-    <h2 class="font-bold text-gray-700 text-center mt-20 mb-16 text-2xl lg:text-4xl">Recorded Events</h2>
-    <div class='grid grid-cols-3 gap-6 justify-center'>
-    {#each getCurrentPageData() as blogData, index}
-    <BlogCard1 
-        title={blogData.title}
-        date={blogData.date}
-        description={blogData.description}
-        image={blogData.image}
-        imageAlt={blogData.imageAlt}
-        slug={blogData.slug}
-     />
-    {/each}
     
+    <div class="flex justify-start space-x-4 mb-10">
+        <button class={`px-4 py-2 rounded-2xl ${selectedTab === 'All' ? 'bg-green-500 text-white' : 'bg-gray-300'}`} on:click={() => selectedTab = 'All'}>All</button>
+        <button class={`px-4 py-2 rounded-2xl ${selectedTab === 'Recorded' ? 'bg-green-500 text-white' : 'bg-gray-300'}`} on:click={() => selectedTab = 'Recorded'}>Recorded</button>
+        <button class={`px-4 py-2 rounded-2xl ${selectedTab === 'Upcoming' ? 'bg-green-500 text-white' : 'bg-gray-300'}`} on:click={() => selectedTab = 'Upcoming'}>Upcoming</button>
     </div>
 
-    <div class="flex justify-center items-center mt-10 space-x-2">
-        <button on:click={prevPage} disabled={currentPage === 1} class="px-4 py-2 bg-gray-300 rounded disabled:bg-gray-200">Prev</button>
-        
-        {#each Array(totalPages) as _, pageIndex}
-          <button on:click={() => goToPage(pageIndex + 1)} class="px-4 py-2 bg-gray-300 rounded {currentPage === (pageIndex + 1) ? 'bg-blue-500 text-white' : ''}">
-            {pageIndex + 1}
-          </button>
+    <article class='grid grid-cols-1 md:grid-cols-3  2xl:grid-cols-5 my-10  gap-6 justify-center'>
+        {#each filteredData as blogData, index}
+        <BlogCard1 
+            title={blogData.title}
+            date={blogData.date}
+            description={blogData.description}
+            image={blogData.image}
+            imageAlt={blogData.imageAlt}
+            event={blogData.event}
+            duration={blogData.duration}
+            buttonName={blogData.buttonName}
+            slug={blogData.slug}
+         />
         {/each}
         
-        <button on:click={nextPage} disabled={currentPage === totalPages} class="px-4 py-2 bg-gray-300 rounded disabled:bg-gray-200">Next</button>
-      </div>
-
+    </article>
 
 </section>
